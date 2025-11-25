2eeeda6f68892b934ad39ad19f47912f78bcd67d
ThuvaMathu <thuvamathu618@gmail.com>
2025-11-26 01:08:42 +1000

    logo issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
           }}
         />
         {/* Favicon */}
-        <link rel="icon" href="/logos/logo.png" type="image/svg+xml" />
+        <link rel="icon" href="/logos/logo.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body
